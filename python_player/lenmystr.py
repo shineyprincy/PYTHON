2bcf8678d55a9737195492e090150be615c2e8fc
shineyprincy <32951468+shineyprincy@users.noreply.github.com>
2017-10-21 11:29:49 +0530

    Add files via upload

---
+++ python_player/lenmystr.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ mystring="programmer"
+ print(len(mystring))
+ pass
+
+if __name__ == '__main__':
+    main()
