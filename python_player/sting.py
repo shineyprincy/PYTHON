ab3334dc16158b09007781027ea3969532079037
shineyprincy <32951468+shineyprincy@users.noreply.github.com>
2017-10-21 12:15:29 +0530

    Add files via upload

---
+++ python_player/sting.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ string="Hello world"
+ print(len(string))
+ pass
+if __name__ == '__main__':
+    main()
