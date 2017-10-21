a4cb50e014e3ea7edc5e3e750907740df6ec089a
shineyprincy <32951468+shineyprincy@users.noreply.github.com>
2017-10-21 12:14:44 +0530

    Add files via upload

---
+++ python_player/count find replace.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+      mystring="programmer"
+      print(mystring.count('r'))
+      print(mystring.find('g'))
+      print(mystring.replace('m','y'))
+      pass
+
+if __name__ == '__main__':
+    main()
