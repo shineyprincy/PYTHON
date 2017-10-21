307cc21a2a942a40a47dc58a7446977ac4fd950a
shineyprincy <32951468+shineyprincy@users.noreply.github.com>
2017-10-21 09:48:19 +0530

    Add files via upload

---
+++ python_player/mathematics.py
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    a=20
+    b=5
+    c=a+b
+    d=a-b
+    e=a/b
+    f=a%b
+    g=a*b
+    h=a**b
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
+    print(h)
